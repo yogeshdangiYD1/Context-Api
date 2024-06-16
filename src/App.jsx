83f785d8b6547749fe31e608e911660dd32bfc07
Yogesh Dangi <82973350+yogeshdangiYD1@users.noreply.github.com>
2024-06-17 02:39:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import UserContextProvider from './context/UserContextProvider'
+import Profile from './components/Profile'
+import Login from './components/Login'
+
+
+function App() {
+
+  return (
+    <UserContextProvider>
+      <h1>Context api</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ src/context/UserContextProvider.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UserContext from './UserContext'
+
+const UserContextProvider = ({ childern }) => {
+    const [user, setUser] = React.useState(null)
+    return (
+        <UserContext.Provider value={{user, setUser}}>
+        {childern}
+        </UserContext.Provider>
+    )
+}
+
+export default UserContextProvider; 
